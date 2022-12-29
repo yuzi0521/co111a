f19743ea45f2246ef11a60713086dd8fc1b3994d
yuzi0521 <a25943852@gmail.com>
2022-12-29 10:34:47 +0800

    update 1229

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//按照老師課堂上講的，先將R0*R1=R2用C先試著寫出來
+// 令R0 = a
+// 令R1 = b
+// 令R2 = sum
+// R2=0
+// while(a>0)
+// {
+//   sum += b
+//   a-- 
+// }
+
+@R0
+D=M
+@a
+M=D	// 令R0 = a
+
+@R1
+D=M
+@b
+M=D	// 令R1 = b
+
+@0
+D=A
+@R2
+D=M	
+@sum
+M=D	// 令R2 = sum
+
+(WHILE)
+	@a
+	D=M	
+	@END
+	D;JLE	// if a <= 0 END      
+	
+	@b
+	D=M	// D = y
+	@sum
+	M=D+M	// sum = sum + b
+	@1
+	D=A	// D = 1
+	@a
+	M=M-D	// a = a - 1	
+
+	@WHILE
+	0;JMP	// if a>0 重新回到while迴圈
+(END)	
+	@END
+	0;JMP	
+
+    //from https://github.com/b8990523/co111a/blob/main/04/mult/mult.asm
